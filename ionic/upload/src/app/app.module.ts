6077b107b927f8d606bf473b7d36f5d8d2ab21cd
thaninpong <thaninpong@perfenterprise.com>
2019-06-14 16:44:39 +0700

    add

--- ionic/upload/src/app/app.module.ts
+++ ionic/upload/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    HttpClient,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- ionic/upload/src/pages/home/home.ts
+++ ionic/upload/src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  formData: FormData;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: HttpClient, public loadingCtrl: LoadingController,public alertController: AlertController) {
+
+  }
+  onSubmit() {
+    //http://localhost:5000/api/ElectionV3/UploadFile
+    // https://electionvars.azurewebsites.net/api/ElectionV3/UploadFile
+    // GlobalVaraible.host + "UploadFile"
+    console.log("1");
+    const loader = this.loadingCtrl.create({
+      content: 'กรุณารอสักครู่ กำลังอัปโหลดข้อมูล...',
+      duration: 5000,
+      dismissOnPageChange: true
+    })
+    loader.present();
+      this.http.post("https://localhost:44377/api/Upload/UploadFile", this.formData).subscribe(data => {
+        console.log("2");
+        const confirm = this.alertController.create({
+          title: 'อัปโหลดสำเร็จ',
+          buttons: [
+            {
+              text: 'OK',
+              handler: () => {
+                loader.dismiss();
+                console.log("done");
+              }
+            }
+          ]
+        });
+        confirm.present();
+      });
+
 
   }
 
+  setFile(event) {
+    let files = event.srcElement.files
+    if (files) {
+      this.formData = new FormData()
+      for (let i = 0; i < files.length; i++) {
+        this.formData.append(i.toString(), files[i], files[i].name);
+      }
+    }
+  }
 }
